0f2a6ecebd43a5b97f0a83a10f4ffb0702ab47af
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-18 12:03:04 +0530

    Refactored:Controller Merge ForgetPasswordController To UserController

--- src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
@@ -1,27 +1,56 @@
 package com.bridgelabz.lmsapplication.controller;
 
+import com.bridgelabz.lmsapplication.dto.EmailDto;
 import com.bridgelabz.lmsapplication.dto.UserDto;
+import com.bridgelabz.lmsapplication.model.UserDetail;
+import com.bridgelabz.lmsapplication.security.JwtTokenUtil;
 import com.bridgelabz.lmsapplication.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.mail.MessagingException;
+
+@Controller("/user")
 public class UserController {
 
     @Autowired
-    JwtUserDetailsService userDetailsService;
+    JwtUserDetailsService service;
 
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    //API FOR USER LOGIN
     @RequestMapping({"/login"})
     public String login() {
         return "Login Successfully";
     }
 
+    //API FOR REGISTER USER IN LMS APPLICATION
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> saveUser(@RequestBody UserDto user) {
-        return ResponseEntity.ok(userDetailsService.loadUserDetails(user));
+    public ResponseEntity<?> register(@RequestBody UserDto user) {
+        return ResponseEntity.ok(service.loadUserDetails(user));
+    }
+
+    //FORGET PASSWORD API
+    @RequestMapping(value = "/send", method = RequestMethod.POST)
+    public String sentMail(@RequestBody EmailDto emailDto) throws MessagingException {
+        UserDetail user = service.findByEmail(emailDto.getEmailId());
+        final String token = jwtTokenUtil.generateEmailToken(user.getId());
+        service.sendEmail(emailDto, token);
+        return "Mail Send Successfully";
+    }
+
+    //RESET PASSWORD API
+    @RequestMapping(value = "/reset", method = RequestMethod.PUT)
+    public String resetPassword(@RequestParam(value = "password") String password, @RequestParam(value = "token") String token) {
+        String id = jwtTokenUtil.getUsernameFromToken(token);
+        service.resetPassword(Long.valueOf(id), passwordEncoder.encode(password));
+        return "Password Changed";
     }
 }
