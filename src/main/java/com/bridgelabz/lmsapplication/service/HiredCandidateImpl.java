d9c88b5f4b8de89902b3ac4979b6d491ae7b0d92
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-19 20:26:04 +0530

    Added:Implementation For Service Class

--- src/main/java/com/bridgelabz/lmsapplication/service/HiredCandidateImpl.java
+++ src/main/java/com/bridgelabz/lmsapplication/service/HiredCandidateImpl.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.lmsapplication.service;
 
+import com.bridgelabz.lmsapplication.dto.HiredCandidateDto;
+import com.bridgelabz.lmsapplication.model.HiredCandidateModel;
 import com.bridgelabz.lmsapplication.repository.CandidateRepository;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -12,8 +24,76 @@ public class HiredCandidateImpl implements HiredCandidateService {
     @Autowired
     CandidateRepository repository;
 
+    @Autowired
+    ModelMapper mapper;
+
+    HiredCandidateDto hiredCandidateDto = new HiredCandidateDto();
+
     @Override
     public List getHiredCandidates() {
         return repository.findAll();
+    }
+
+    public List loadExcelData() throws IOException {
+        String path = "./src/main/resources/HiredCandidates.xlsx";
+        boolean flag = true;
+        List sheetData = new ArrayList();
+        HiredCandidateDto hiredCandidateDto = new HiredCandidateDto();
+        try (FileInputStream fis = new FileInputStream(path)) {
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator rows = sheet.rowIterator();
+            XSSFCell cell;
+            while (rows.hasNext()) {
+                XSSFRow row = (XSSFRow) rows.next();
+                Iterator cells = row.cellIterator();
+                if (flag == false) {
+                    while (cells.hasNext()) {
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setId((long) cell.getNumericCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setFirst_name(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setMiddle_name(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setLast_name(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setEmail_id(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setHired_city(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setDegree(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setHired_date(cell.getDateCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setMobile_number((long) cell.getNumericCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setPermanent_pincode((long) cell.getNumericCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setHired_lab(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setAttitude(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setCommunication_remark(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setKnowledge_remark(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setAggregate_remark(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setStatus(cell.getStringCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setCreator_stamp(cell.getDateCellValue());
+                        cell = (XSSFCell) cells.next();
+                        hiredCandidateDto.setCreator_user(cell.getStringCellValue());
+                        HiredCandidateModel hiredCandidate = mapper.map(hiredCandidateDto, HiredCandidateModel.class);
+                        repository.save(hiredCandidate);
+                    }
+                }
+                flag = false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sheetData;
     }
 }
