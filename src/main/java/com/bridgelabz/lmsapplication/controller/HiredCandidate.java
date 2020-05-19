4db3dbf5102921050815a574831a173f581b53e1
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-19 20:28:56 +0530

    Added:Controller For Load Excel Sheet Data in Database

--- src/main/java/com/bridgelabz/lmsapplication/controller/HiredCandidate.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/HiredCandidate.java
@@ -3,8 +3,10 @@ package com.bridgelabz.lmsapplication.controller;
 import com.bridgelabz.lmsapplication.service.HiredCandidateImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,5 +17,11 @@ public class HiredCandidate {
     @GetMapping("/hiredCandidateList")
     public List getHiredCandidate() {
         return service.getHiredCandidates();
+    }
+
+    @PostMapping("/read")
+    public String read() throws IOException {
+        service.loadExcelData();
+        return "sucess";
     }
 }
