b15ead76464c119a2bde62cf1f6445977b89d00e
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-20 07:28:23 +0530

    Modified:Indentation

--- src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.mail.MessagingException;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
