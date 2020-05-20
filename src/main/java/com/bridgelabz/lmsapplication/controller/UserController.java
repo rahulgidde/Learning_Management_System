73b8d096d0761f0835071e0c29a9c4a88bc390fa
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-20 19:44:05 +0530

    Modified:Removed Unnecessary Code

--- src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/UserController.java
@@ -3,10 +3,8 @@ package com.bridgelabz.lmsapplication.controller;
 import com.bridgelabz.lmsapplication.dto.UserDto;
 import com.bridgelabz.lmsapplication.model.JwtRequest;
 import com.bridgelabz.lmsapplication.service.IUserService;
-import com.bridgelabz.lmsapplication.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -17,12 +15,6 @@ public class UserController {
 
     @Autowired
     private IUserService userService;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     //API FOR AUTHENTICATE USER
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
@@ -52,8 +44,7 @@ public class UserController {
     //RESET PASSWORD API
     @RequestMapping(value = "/resetpassword", method = RequestMethod.PUT)
     public String resetPassword(@RequestParam(value = "password") String password, @RequestParam(value = "token") String token) {
-        String id = jwtTokenUtil.getUsernameFromToken(token);
-        userService.resetPassword(Long.valueOf(id), passwordEncoder.encode(password));
+        userService.resetPassword(token, password);
         return "Password Changed Successfully";
     }
 }
