0c036a94169b608bbb2184db8df09bdaa8a76c22
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-20 19:45:36 +0530

    Modified:Changes Method Data Type

--- src/main/java/com/bridgelabz/lmsapplication/service/IUserService.java
+++ src/main/java/com/bridgelabz/lmsapplication/service/IUserService.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.lmsapplication.service;
 
-import com.bridgelabz.lmsapplication.dto.EmailDto;
 import com.bridgelabz.lmsapplication.dto.UserDto;
 import com.bridgelabz.lmsapplication.model.JwtRequest;
 import com.bridgelabz.lmsapplication.model.UserDetailModel;
@@ -17,7 +16,7 @@ public interface IUserService {
 
     public UserDetailModel findByEmail(String email);
 
-    public UserDetailModel resetPassword(Long id, String password);
+    public UserDetailModel resetPassword(String token, String password);
 
     public void sendEmail(String emailId) throws MessagingException;
 }
