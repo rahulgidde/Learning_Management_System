3e8b6939a23f9eb1658f89550f22541cc93f09e2
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-22 22:11:11 +0530

    Modified:Indentation

--- src/main/java/com/bridgelabz/lmsapplication/service/IUserService.java
+++ src/main/java/com/bridgelabz/lmsapplication/service/IUserService.java
@@ -3,13 +3,12 @@ package com.bridgelabz.lmsapplication.service;
 import com.bridgelabz.lmsapplication.dto.UserDto;
 import com.bridgelabz.lmsapplication.model.JwtRequest;
 import com.bridgelabz.lmsapplication.model.UserDetailModel;
-import org.springframework.http.ResponseEntity;
 
 import javax.mail.MessagingException;
 import java.util.Optional;
 
 public interface IUserService {
-    public ResponseEntity<?> createAuthenticationToken(JwtRequest authenticationRequest) throws Exception;
+    public String createAuthenticationToken(JwtRequest authenticationRequest) throws Exception;
 
     public void authenticate(String username, String password) throws Exception;
 
