146f2b37c2bf8710c3e46385150a4bb71831ec49
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-26 11:52:29 +0530

    Added:JapRepository For Fellowship Candidate

--- src/main/java/com/bridgelabz/lmsapplication/repository/FellowshipRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/FellowshipRepository.java
@@ -1,4 +1,9 @@
 package com.bridgelabz.lmsapplication.repository;
 
-public class FellowshipRepository {
+import com.bridgelabz.lmsapplication.model.FellowshipModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FellowshipRepository extends JpaRepository<FellowshipModel,Long> {
 }
