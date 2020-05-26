a39a171defd3c9d104ea67ee975b747c36500dba
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-26 12:25:01 +0530

    Added:Method For Get Fellowship Candidate Count

--- src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
@@ -15,9 +15,16 @@ public class FellowshipController {
     @Autowired
     IFellowshipService fellowshipService;
 
+    //API FOR COPY ONE TABLE DATA TO ANOTHER TABLE
     @RequestMapping(value = "/fellowshipcandidatesdata", method = RequestMethod.POST)
     public ResponseEntity<String> getFellowshipCandidateData() {
         fellowshipService.fellowshipCandidatesData();
         return new ResponseEntity<>("Data Copied Successfully", HttpStatus.OK);
+    }
+
+    //API FOR GET CANDIDATE COUNT
+    @RequestMapping(value = "/fellowshipcandidatecount", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getFellowshipCandidateCount() {
+        return new ResponseEntity<>(fellowshipService.FellowshipCandidateCount(), HttpStatus.OK);
     }
 }
