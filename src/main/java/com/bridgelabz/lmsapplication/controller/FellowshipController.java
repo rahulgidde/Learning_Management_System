ef487527394d8687ba18387dbd1699e807e453c0
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-26 12:01:30 +0530

    Added:Fellowship Controller

--- src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.lmsapplication.controller;
 
+import com.bridgelabz.lmsapplication.service.IFellowshipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fellowship")
 public class FellowshipController {
+
+    @Autowired
+    IFellowshipService fellowshipService;
+
+    @RequestMapping(value = "/fellowshipcandidatesdata", method = RequestMethod.POST)
+    public ResponseEntity<String> getFellowshipCandidateData() {
+        fellowshipService.fellowshipCandidatesData();
+        return new ResponseEntity<>("Data Copied Successfully", HttpStatus.OK);
+    }
 }
