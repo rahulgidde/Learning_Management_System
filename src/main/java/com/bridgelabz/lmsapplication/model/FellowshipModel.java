3dbca0baa6646681f94f26f333ee371547a92cfc
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-26 11:51:30 +0530

    Added:Fellowship Model For Fellowship Candidate

--- src/main/java/com/bridgelabz/lmsapplication/model/FellowshipModel.java
+++ src/main/java/com/bridgelabz/lmsapplication/model/FellowshipModel.java
@@ -1,4 +1,39 @@
 package com.bridgelabz.lmsapplication.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity(name = "fellowship_candidate")
+@Table
 public class FellowshipModel {
+    @Id
+    Long id;
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String email;
+    Integer contactNumber;
+    Date creatorStamp;
+    String creatorUser;
+    Date birthDate;
+    String verifyBirthDate;
+    String parentName;
+    String parentOccupation;
+    int parentMobileNumber;
+    int parentAnnualSalary;
+    String localAddress;
+    String permanentAddress;
+    String photoPath;
+    Date joiningDate;
+    String candidateStatus;
+    String documentStatus;
+    String remark;
 }
