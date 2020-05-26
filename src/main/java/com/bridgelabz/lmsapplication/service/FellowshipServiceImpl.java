b7ee63153d99d5767fcff9d9a3b4b4f468be6b0e
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-26 12:00:48 +0530

    Added:Service Implementation For Fellowship Candidate

--- src/main/java/com/bridgelabz/lmsapplication/service/FellowshipServiceImpl.java
+++ src/main/java/com/bridgelabz/lmsapplication/service/FellowshipServiceImpl.java
@@ -1,4 +1,34 @@
 package com.bridgelabz.lmsapplication.service;
 
-public class FellowshipServiceImpl {
+import com.bridgelabz.lmsapplication.model.FellowshipModel;
+import com.bridgelabz.lmsapplication.repository.CandidateRepository;
+import com.bridgelabz.lmsapplication.repository.FellowshipRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.ListIterator;
+
+@Service
+public class FellowshipServiceImpl implements IFellowshipService {
+    @Autowired
+    CandidateRepository candidateRepository;
+
+    @Autowired
+    FellowshipRepository fellowshipRepository;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public void fellowshipCandidatesData() {
+        List list = candidateRepository.findAll();
+        ListIterator iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            FellowshipModel fellowshipModel = mapper.map(iterator.next(), FellowshipModel.class);
+            if (fellowshipModel.getCandidateStatus().equals("Accept"))
+                fellowshipRepository.save(fellowshipModel);
+        }
+    }
 }
