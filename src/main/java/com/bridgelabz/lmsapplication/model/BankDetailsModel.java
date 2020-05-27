5dcbf2727d829919a2d8b32264a9f211e9c03aaa
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-27 18:36:56 +0530

    Added:Bank Model

--- src/main/java/com/bridgelabz/lmsapplication/model/BankDetailsModel.java
+++ src/main/java/com/bridgelabz/lmsapplication/model/BankDetailsModel.java
@@ -1,4 +1,28 @@
 package com.bridgelabz.lmsapplication.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity(name = "candidate_bank_details")
+@Table
 public class BankDetailsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    Long candidateId;
+    String name;
+    int accountNumber;
+    String accountNumberVerified;
+    String ifscCode;
+    String ifscCodeVerified;
+    int panNumber;
+    int addhaarNumber;
+    String addhaarNumberVerified;
+    Date creatorStamp;
+    String creatorUser;
 }
