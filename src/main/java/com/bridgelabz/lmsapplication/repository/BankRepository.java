b3aa78331fd4b93b1fa08fded080471d2687040c
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-27 18:39:15 +0530

    Modified:Indentation

--- src/main/java/com/bridgelabz/lmsapplication/repository/BankRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/BankRepository.java
@@ -1,4 +1,9 @@
 package com.bridgelabz.lmsapplication.repository;
 
-public interface BankRepository {
+import com.bridgelabz.lmsapplication.model.BankDetailsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankRepository extends JpaRepository<BankDetailsModel, Long> {
 }
--- src/main/java/com/bridgelabz/lmsapplication/repository/FellowshipRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/FellowshipRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FellowshipRepository extends JpaRepository<FellowshipModel,Long> {
+public interface FellowshipRepository extends JpaRepository<FellowshipModel, Long> {
 }
