8f36f1f25579f77986458bd6ec480794e5459609
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-28 11:59:10 +0530

    Added:Qualification Repository

--- src/main/java/com/bridgelabz/lmsapplication/repository/QualificationRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/QualificationRepository.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.lmsapplication.repository;
 
-import com.bridgelabz.lmsapplication.model.EducationalInfoModel;
+import com.bridgelabz.lmsapplication.model.QualificationModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EducationRepository extends JpaRepository<EducationalInfoModel,Long> {
+public interface QualificationRepository extends JpaRepository<QualificationModel,Long> {
 }
