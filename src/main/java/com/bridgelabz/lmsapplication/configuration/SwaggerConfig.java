1518d07bd5afa3cd41615758d0b1f9f78c6d9dfe
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-29 10:19:01 +0530

    Modified:Changes Swagger Configuration

--- src/main/java/com/bridgelabz/lmsapplication/configuration/SwaggerConfig.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/SwaggerConfig.java
@@ -2,12 +2,11 @@ package com.bridgelabz.lmsapplication.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @Configuration
@@ -16,9 +15,10 @@ public class SwaggerConfig {
     @Bean
     public Docket swagger() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("8082")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.bridgelabz.lmsapplication"))
-                .paths(regex("/rest.*"))
+                .paths(PathSelectors.any())
                 .build();
     }
 }
