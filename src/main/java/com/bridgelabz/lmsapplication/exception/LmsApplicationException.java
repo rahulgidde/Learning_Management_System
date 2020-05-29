c9921956f5566d86b1e7a4b81ff797ff1e88c9ec
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-29 09:20:15 +0530

    Modified:Uses ControllerAdvice Annotation

--- src/main/java/com/bridgelabz/lmsapplication/exception/LmsApplicationException.java
+++ src/main/java/com/bridgelabz/lmsapplication/exception/LmsApplicationException.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
+@ControllerAdvice
 public class LmsApplicationException extends ResponseEntityExceptionHandler {
     //GLOBAL EXCEPTION HANDLER
     @ExceptionHandler(UserException.class)
