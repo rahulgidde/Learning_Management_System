517755955b59787f18a3ccf2d183cb2c31abee38
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-29 07:40:35 +0530

    Added:Method For Find EmailId

--- src/main/java/com/bridgelabz/lmsapplication/repository/CandidateRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/CandidateRepository.java
@@ -4,6 +4,9 @@ import com.bridgelabz.lmsapplication.model.HiredCandidateModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CandidateRepository extends JpaRepository<HiredCandidateModel, Long> {
+    Optional<HiredCandidateModel> findByEmailId(String emailId);
 }
