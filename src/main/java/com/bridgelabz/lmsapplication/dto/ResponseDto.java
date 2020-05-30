4b6bcd2c43e136fce521696078e49a58e0328109
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-30 18:38:59 +0530

    Added:Response Dto

--- src/main/java/com/bridgelabz/lmsapplication/dto/ResponseDto.java
+++ src/main/java/com/bridgelabz/lmsapplication/dto/ResponseDto.java
@@ -1,4 +1,11 @@
 package com.bridgelabz.lmsapplication.dto;
 
 public class ResponseDto {
+    public Object data;
+    public String message;
+
+    public ResponseDto(Object data, String message) {
+        this.data = data;
+        this.message = message;
+    }
 }
