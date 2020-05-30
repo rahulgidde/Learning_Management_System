0ec2bb4c84d7bae8dd813c1f92817ab79f50e3f4
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-30 18:44:40 +0530

    Refactored:Code Changes Response Type Uses ResponseDto For Response

--- src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
+++ src/main/java/com/bridgelabz/lmsapplication/controller/FellowshipController.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.lmsapplication.controller;
 
+import com.bridgelabz.lmsapplication.configuration.ApplicationConfiguration;
 import com.bridgelabz.lmsapplication.dto.PersonalInfoDto;
+import com.bridgelabz.lmsapplication.dto.ResponseDto;
+import com.bridgelabz.lmsapplication.model.FellowshipModel;
 import com.bridgelabz.lmsapplication.service.IFellowshipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,27 +19,30 @@ public class FellowshipController {
 
     //API FOR COPY ONE TABLE DATA TO ANOTHER TABLE
     @RequestMapping(value = "/fellowshipcandidatesdata", method = RequestMethod.POST)
-    public ResponseEntity<String> getFellowshipCandidateData() {
-        fellowshipService.fellowshipCandidatesData();
-        return new ResponseEntity<>("Data Copied Successfully", HttpStatus.ACCEPTED);
+    public ResponseEntity<ResponseDto> fellowshipCandidatesData() {
+        return new ResponseEntity<>(new ResponseDto(fellowshipService.fellowshipCandidatesData(),
+                ApplicationConfiguration.getMessageAccessor().getMessage("109")), HttpStatus.ACCEPTED);
     }
 
     //API FOR GET CANDIDATE COUNT
     @RequestMapping(value = "/fellowshipcandidatecount", method = RequestMethod.GET)
-    public ResponseEntity<Integer> getFellowshipCandidateCount() {
-        return new ResponseEntity<>(fellowshipService.FellowshipCandidateCount(), HttpStatus.FOUND);
+    public ResponseEntity<ResponseDto> fellowshipCandidateCount() {
+        return new ResponseEntity<>(new ResponseDto(fellowshipService.fellowshipCandidateCount(),
+                ApplicationConfiguration.getMessageAccessor().getMessage("113")), HttpStatus.FOUND);
     }
 
     //API FOR SEND JOB OFFER MAIL
     @RequestMapping(value = "/sendjoboffer", method = RequestMethod.POST)
-    public ResponseEntity<String> getJobOfferMail() {
-        fellowshipService.jobOfferMail();
-        return new ResponseEntity<>("Mail Send Successfully", HttpStatus.OK);
+    public ResponseEntity<ResponseDto> sendJobOfferMail() {
+        return new ResponseEntity<>(new ResponseDto(fellowshipService.jobOfferMail(),
+                ApplicationConfiguration.getMessageAccessor().getMessage("103")), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/updatepersonalnfo", method = RequestMethod.PUT)
-    public ResponseEntity<String> updatePersonalInfo(@RequestParam(value = "id") Long id, @RequestBody PersonalInfoDto personalInfoDto) {
-        fellowshipService.personalInfo(id, personalInfoDto);
-        return new ResponseEntity<>("Personal Information Updated", HttpStatus.FOUND);
+    public ResponseEntity<ResponseDto> updateCandidatePersonalInfo(@RequestParam(value = "id") Long id,
+                                                                   @RequestBody PersonalInfoDto personalInfoDto) {
+        FellowshipModel fellowshipModel = fellowshipService.personalInfo(id, personalInfoDto);
+        return new ResponseEntity<>(new ResponseDto(fellowshipModel, ApplicationConfiguration.getMessageAccessor()
+                .getMessage("114")), HttpStatus.FOUND);
     }
 }
