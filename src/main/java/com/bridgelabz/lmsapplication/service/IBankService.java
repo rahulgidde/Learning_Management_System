29e6967aae32b0a7bc673b655bc5f5963adcc9b9
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-31 11:30:55 +0530

    Added:Message

--- src/main/java/com/bridgelabz/lmsapplication/service/IBankService.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.lmsapplication.service;
-
-import com.bridgelabz.lmsapplication.dto.BankDetailsDto;
-
-public interface IBankService {
-    public void bankDetails(BankDetailsDto bankDetailsDto);
-}
--- src/main/java/com/bridgelabz/lmsapplication/service/IEducationService.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.lmsapplication.service;
-
-import com.bridgelabz.lmsapplication.dto.QualificationDto;
-
-public interface IEducationService {
-    void educationalInfo(QualificationDto qualificationDto);
-}
