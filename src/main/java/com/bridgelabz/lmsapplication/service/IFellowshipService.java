d95b6f9f9a9968de5828da15304d3c9f185472b2
rahulgidde.com <rahulgidde1212@gmail.com>
2020-05-31 11:34:14 +0530

    Refactored:Code Added Methods For Get candidate Information

--- src/main/java/com/bridgelabz/lmsapplication/service/IFellowshipService.java
+++ src/main/java/com/bridgelabz/lmsapplication/service/IFellowshipService.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.lmsapplication.service;
 
+import com.bridgelabz.lmsapplication.dto.BankDetailsDto;
 import com.bridgelabz.lmsapplication.dto.PersonalInfoDto;
+import com.bridgelabz.lmsapplication.dto.QualificationDto;
+import com.bridgelabz.lmsapplication.model.BankDetailsModel;
 import com.bridgelabz.lmsapplication.model.FellowshipModel;
+import com.bridgelabz.lmsapplication.model.QualificationModel;
 
 public interface IFellowshipService {
     public boolean fellowshipCandidatesData();
@@ -11,4 +15,8 @@ public interface IFellowshipService {
     public boolean jobOfferMail();
 
     public FellowshipModel personalInfo(Long id, PersonalInfoDto personalInfoDto);
+
+    public BankDetailsModel bankDetails(BankDetailsDto bankDetailsDto);
+
+    public QualificationModel educationalInfo(QualificationDto qualificationDto);
 }
