6712b90cf911e7c6b5149e368e6644c551a8c259
rahulgidde.com <rahulgidde1212@gmail.com>
2020-06-01 23:00:07 +0530

    Added:Document Repository

--- src/main/java/com/bridgelabz/lmsapplication/repository/DocumentRepository.java
+++ src/main/java/com/bridgelabz/lmsapplication/repository/DocumentRepository.java
@@ -4,4 +4,5 @@ import com.bridgelabz.lmsapplication.model.DocumentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DocumentRepository extends JpaRepository<DocumentModel, Long> {
+
 }
