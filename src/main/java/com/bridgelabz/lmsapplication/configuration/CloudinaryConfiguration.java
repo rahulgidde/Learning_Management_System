c8ce30cd4428ae72871f26d3f5e0c671737b8e9d
rahulgidde <rahulgidde1212@gmail.com>
2020-06-05 16:58:51 +0530

    Added:Cloudinary Configration

--- src/main/java/com/bridgelabz/lmsapplication/configuration/CloudinaryConfiguration.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/CloudinaryConfiguration.java
@@ -8,14 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class CloudinaryConfig {
+public class CloudinaryConfiguration {
     @Bean
     public Cloudinary cloudinaryConfig() {
         Cloudinary cloudinary = null;
         Map config = new HashMap();
-        config.put("cloud_name", System.getenv().get("cloudinary_cloud_name"));
-        config.put("api_key", System.getenv().get("cloudinary-api_key"));
-        config.put("api_secret",  System.getenv().get("cloudinary_api_secret"));
+        config.put("cloud_name", System.getenv().get("cloudinary.cloud_name"));
+        config.put("api_key", System.getenv().get("cloudinary.api_key"));
+        config.put("api_secret", System.getenv().get("cloudinary.api_secret"));
         cloudinary = new Cloudinary(config);
         return cloudinary;
     }
