3f0a6d256b1d79424b607520f990f5316e5db7bf
rahulgidde <rahulgidde1212@gmail.com>
2020-06-05 16:59:55 +0530

    Added:Database Configration

--- src/main/java/com/bridgelabz/lmsapplication/configuration/DatabaseConfiguration.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/DatabaseConfiguration.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.lmsapplication.configuration;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DatabaseConfiguration {
+    @Bean
+    public DataSource getDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(System.getenv().get("spring.datasource.url"));
+        dataSourceBuilder.username(System.getenv().get("spring.datasource.username"));
+        dataSourceBuilder.password(System.getenv().get("spring.datasource.password"));
+        return dataSourceBuilder.build();
+    }
 }
