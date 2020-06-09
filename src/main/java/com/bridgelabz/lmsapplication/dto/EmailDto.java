4cf23a914ddb876ede9c45d6871574ada295048e
rahulgidde <rahulgidde1212@gmail.com>
2020-06-09 09:19:54 +0530

    Modified:UsesLambok

--- src/main/java/com/bridgelabz/lmsapplication/dto/EmailDto.java
+++ src/main/java/com/bridgelabz/lmsapplication/dto/EmailDto.java
@@ -1,31 +1,12 @@
 package com.bridgelabz.lmsapplication.dto;
 
-public class EmailDto {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class EmailDto implements Serializable {
     private String emailId;
-    private String subject = "Forget Password";
-    private String body = "Reset Password";
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
+    private String subject;
+    private String body;
 }
