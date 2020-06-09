9a6e204ee81cf9dcfee927f473a5fba76317fb2e
rahulgidde <rahulgidde1212@gmail.com>
2020-06-09 08:57:58 +0530

    Added:Redis Configuration

--- src/main/java/com/bridgelabz/lmsapplication/configuration/RedisConfig.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/RedisConfig.java
@@ -1,4 +1,33 @@
 package com.bridgelabz.lmsapplication.configuration;
 
-public class RedisConfig {
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory(){
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(System.getenv().get("hostname"));
+        jedisConnectionFactory.setPort(Integer.parseInt(System.getenv().get("port")));
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    RedisTemplate<String,Object> getRedisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
 }
