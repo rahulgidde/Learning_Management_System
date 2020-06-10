8b00fdd0d2451bc2632c898ce059a4aa6e4aedbe
rahulgidde <rahulgidde1212@gmail.com>
2020-06-10 13:54:12 +0530

    Modified:Changs Variables Uses Lambok

--- src/main/java/com/bridgelabz/lmsapplication/dto/EmailDto.java
+++ src/main/java/com/bridgelabz/lmsapplication/dto/EmailDto.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.lmsapplication.dto;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
+@Component
 @Data
 public class EmailDto implements Serializable {
     private String emailId;
