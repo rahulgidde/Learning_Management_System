1e994e8a62a71f5796ea0275042b2c60060427db
rahulgidde <rahulgidde1212@gmail.com>
2020-06-10 13:51:49 +0530

    Modified:Changes Configuration Remove Beans

--- src/main/java/com/bridgelabz/lmsapplication/configuration/RabbitMqConfig.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/RabbitMqConfig.java
@@ -1,10 +1,6 @@
 package com.bridgelabz.lmsapplication.configuration;
 
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,17 +30,5 @@ public class RabbitMqConfig {
     @Bean
     Binding binding(Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-    }
-
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public AmqpTemplate rabbitMq(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
     }
 }
