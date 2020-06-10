e5cad0c89a9d5683868dee941725c512ad3d7c2c
rahulgidde <rahulgidde1212@gmail.com>
2020-06-10 13:55:02 +0530

    Added:Exception Type

--- src/main/java/com/bridgelabz/lmsapplication/exception/UserException.java
+++ src/main/java/com/bridgelabz/lmsapplication/exception/UserException.java
@@ -9,6 +9,6 @@ public class UserException extends RuntimeException {
     }
 
     public enum exceptionType {
-        User_Not_FOUND, INVALID_EMAIL_ID, FiLE_NOT_FOUND;
+        User_Not_FOUND, INVALID_EMAIL_ID, FiLE_NOT_FOUND, INVALID_TOKEN;
     }
 }
