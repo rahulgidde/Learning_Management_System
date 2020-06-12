9eade0f994f026581281f94aaf5aa7203b0d5af1
rahulgidde <rahulgidde1212@gmail.com>
2020-06-12 22:09:59 +0530

    Added:AOP Configuration

--- src/main/java/com/bridgelabz/lmsapplication/configuration/LmsAspectConfig.java
+++ src/main/java/com/bridgelabz/lmsapplication/configuration/LmsAspectConfig.java
@@ -1,4 +1,27 @@
 package com.bridgelabz.lmsapplication.configuration;
 
-public class LmsAspectCnfig {
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class LmsAspectConfig {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.bridgelabz.lmsapplication.service.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        logger.info("Check before Method Call");
+        logger.info("Allowed execution for {}", joinPoint);
+    }
+
+    @After("execution(* com.bridgelabz.lmsapplication.service.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        logger.info("Check after Method Call ");
+        logger.info("Allowed execution for {}", joinPoint);
+    }
 }
