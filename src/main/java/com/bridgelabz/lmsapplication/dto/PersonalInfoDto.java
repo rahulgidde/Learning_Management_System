d13ad97ce0d9b81e5cd12dfcd0a0a46ab043fc37
rahulgidde <rahulgidde1212@gmail.com>
2020-06-12 22:50:27 +0530

    Modified:Uses Lambok

--- src/main/java/com/bridgelabz/lmsapplication/dto/PersonalInfoDto.java
+++ src/main/java/com/bridgelabz/lmsapplication/dto/PersonalInfoDto.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.lmsapplication.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class PersonalInfoDto {
     private Date birthDate;
     private String verifyBirthDate;
@@ -12,76 +15,4 @@ public class PersonalInfoDto {
     private String localAddress;
     private String permanentAddress;
     private String photoPath;
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getVerifyBirthDate() {
-        return verifyBirthDate;
-    }
-
-    public void setVerifyBirthDate(String verifyBirthDate) {
-        this.verifyBirthDate = verifyBirthDate;
-    }
-
-    public String getParentName() {
-        return parentName;
-    }
-
-    public void setParentName(String parentName) {
-        this.parentName = parentName;
-    }
-
-    public String getParentOccupation() {
-        return parentOccupation;
-    }
-
-    public void setParentOccupation(String parentOccupation) {
-        this.parentOccupation = parentOccupation;
-    }
-
-    public int getParentMobileNumber() {
-        return parentMobileNumber;
-    }
-
-    public void setParentMobileNumber(int parentMobileNumber) {
-        this.parentMobileNumber = parentMobileNumber;
-    }
-
-    public int getParentAnnualSalary() {
-        return parentAnnualSalary;
-    }
-
-    public void setParentAnnualSalary(int parentAnnualSalary) {
-        this.parentAnnualSalary = parentAnnualSalary;
-    }
-
-    public String getLocalAddress() {
-        return localAddress;
-    }
-
-    public void setLocalAddress(String localAddress) {
-        this.localAddress = localAddress;
-    }
-
-    public String getPermanentAddress() {
-        return permanentAddress;
-    }
-
-    public void setPermanentAddress(String permanentAddress) {
-        this.permanentAddress = permanentAddress;
-    }
-
-    public String getPhotoPath() {
-        return photoPath;
-    }
-
-    public void setPhotoPath(String photoPath) {
-        this.photoPath = photoPath;
-    }
 }
--- src/main/java/com/bridgelabz/lmsapplication/dto/UserDto.java
+++ src/main/java/com/bridgelabz/lmsapplication/dto/UserDto.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.lmsapplication.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class UserDto {
     private Long id;
     private String username;
@@ -12,76 +15,4 @@ public class UserDto {
     private Integer contactNumber;
     private Date creatorStamp;
     private String creatorUser;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(Integer contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public Date getCreatorStamp() {
-        return creatorStamp;
-    }
-
-    public void setCreatorStamp(Date creatorStamp) {
-        this.creatorStamp = creatorStamp;
-    }
-
-    public String getCreatorUser() {
-        return creatorUser;
-    }
-
-    public void setCreatorUser(String creatorUser) {
-        this.creatorUser = creatorUser;
-    }
 }
