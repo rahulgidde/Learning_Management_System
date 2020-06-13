abc915ad212e0dc996abcbdd3716f3f7520c2d30
rahulgidde <rahulgidde1212@gmail.com>
2020-06-13 11:05:56 +0530

    Added:RedisUtil Class

--- src/main/java/com/bridgelabz/lmsapplication/util/RedisUtil.java
+++ src/main/java/com/bridgelabz/lmsapplication/util/RedisUtil.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.lmsapplication.util;
 
-public class RabbitMQ {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class RedisUtil implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String rediskey, String userName, String token) {
+        redisTemplate.opsForHash().put(rediskey, userName, token);
+    }
+
+    public Object getRedisToken(String rediskey, String userName) {
+        return redisTemplate.opsForHash().get(rediskey, userName);
+    }
 }
